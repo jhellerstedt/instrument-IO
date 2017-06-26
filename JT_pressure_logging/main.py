4c4e2c0c761fa7a1e8478ed4d36f49c95246c474
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2017-06-26 14:55:48 +0200

    change layout format

--- JT_pressure_logging/main.py
+++ JT_pressure_logging/main.py
@@ -204,7 +204,7 @@ channel_selection.on_click(change_title)
 
 l = layout([LL_display, prep_display, micro_display], 
             [LL_p, prep_p, micro_p],
-            [hist_p, column(channel_selection, start_date_widget, end_date_widget, update_hist_data)])
+            [hist_p, [[channel_selection], [start_date_widget], [end_date_widget], [update_hist_data]]])
             # sizing_mode='scale_width')
             
 # l2 = column(l, hist_layout)
