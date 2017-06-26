4f86f516da27d1c65418f825986b5887375c369c
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2017-06-26 15:04:19 +0200

    no scale_width, remove hist_p

--- JT_pressure_logging/main.py
+++ JT_pressure_logging/main.py
@@ -204,8 +204,8 @@ hist_widgets = widgetbox(channel_selection, start_date_widget, end_date_widget, 
 
 l = layout([LL_display, prep_display, micro_display], 
             [LL_p, prep_p, micro_p],
-            [hist_p, hist_widgets],
-            sizing_mode='scale_width')
+            [hist_widgets])
+            # sizing_mode='scale_width')
             
 # l2 = column(l, hist_layout)
 curdoc().add_root(l)
