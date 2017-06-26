a90e6b23223c986803f1a79d301a344b4ef034fe
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2017-06-26 15:42:26 +0200

    debug more plot

--- JT_pressure_logging/main.py
+++ JT_pressure_logging/main.py
@@ -204,7 +204,7 @@ hist_widgets = widgetbox(channel_selection, start_date_widget, end_date_widget, 
 
 l = layout([LL_display, prep_display, micro_display], 
             [LL_p, prep_p, micro_p],
-            [hist_p],
+            [LL_p],
             [hist_widgets])
             # sizing_mode='scale_width')
             
