d6edacdd51cb3e1142952f61af14fa552c524b43
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2017-06-28 11:45:50 +0200

    change to multiprocessing in server_lifecycle

--- JT_pressure_logging/server_lifecycle.py
+++ JT_pressure_logging/server_lifecycle.py
@@ -9,6 +9,8 @@ Created on Mon Dec 19 17:29:28 2016
 import threading
 from threading import Thread
 
+import multiprocessing
+
 from tornado import gen
 
 import read_pressures
@@ -17,10 +19,14 @@ def on_server_loaded(server_context):
     # t = Thread(target=read_pressures.update, args=())
     # t.setDaemon(True)
     # t.start()
+    t = multiprocessing.Process(target=read_pressures.update, args=())
+    jobs.append(t)
+    t.start()
     return
     
 def on_server_unloaded(server_context):
     # read_pressures.LL_gauge.TPG_close_serial('\dev\ttyUSB2')
     # read_pressures.prep_gauge.VACOM_close_serial('\dev\ttyUSB0')
     # read_pressures.micro_gauge.VACOM_close_serial('\dev\ttyUSB1')
+    t.join()
     return
