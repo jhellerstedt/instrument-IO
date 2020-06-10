7414b6fb31757105601e9d292a0ae2adb8c0f55e
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2020-06-10 21:12:47 +1000

    change G81 vacom read to use serial read_until

--- G81_standalone_pressure_logging/vacom_MVC3.py
+++ G81_standalone_pressure_logging/vacom_MVC3.py
@@ -19,61 +19,26 @@ this can only be called every 2 SECONDS or the gauge serial communication pukes
 import serial
 import time
 
-global ser
-
-def VACOM_open_serial(serial_address):
-    global ser
+def read_pressure(serial_address):
     try:
         ser = serial.Serial(
-            port=serial_address,
-            baudrate=19200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=2
+                port=serial_address,
+                baudrate=19200,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=2
         )
-    except serial.SerialException as e:
-        print(e)
-        return
-    return
-    
-def VACOM_close_serial(serial_address):
-    global ser
-    try:
-        ser.close()
-    except serial.SerialException as e:
-        print(e)
-        return
-    return
-
-
-def VACOM_read_pressure():
-    pressure = '10'
-    
-    try:   
         read_command = "RPV1,\r,OK" ## for VACOM MVC-3
         read_command = bytes(read_command, 'utf-8')
-           
-        tries = 0
-    
-        while pressure[0] != '0' and tries<1:
-            ser.write(read_command)
-            time.sleep(.3)
-            pressure = ''
-            ser_tries = 0
-            while ser.inWaiting() > 0 and ser_tries < 15:
-                pressure += ser.read(1).decode('utf-8')
-                ser_tries = ser_tries+1      
-            tries = tries + 1
-            time.sleep(.2)
         
-    except serial.SerialException as e:
-        print(e)
-        return float('nan')
-    try:
-        if pressure[0] == '0':
-            return float(pressure[3:-1])
-        else:
-            return float('nan')
+        ser.write(read_command)
+        time.sleep(.3)
+        answer = ser.read_until('\r').decode('utf-8')
+        
+        pressure = answer.split('\t')[1]
+        pressure = float(pressure.split('\r')[0])
     except:
-        return float('nan')
+        pressure = float('nan')
+    
+    return pressure
