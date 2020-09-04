fb17a3285da1cb1a68540b9f4c6b6f1984c11444
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2020-09-04 11:22:03 +1000

    cleaned up GP350 read script, copied to G80

--- GP350.py
+++ GP350.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on 29 Jan 2020
+2020-9-4
 
-independent function for reading GP350 pressure over a serial connection
+GP350 ion gauge controller serial pressure read
 
-call function by pressure gauge serial address
-
-function returns pressure on gauge as a float
-otherwise it pukes and returns 0
-
-NOTE
-this can only be called every 2 SECONDS or the gauge serial communication pukes
-
-@author: jack/ Travis Hartley
+author: Jack
 """
 
 import serial
 import time
 
-global ser
 
-def GP350_open_serial(serial_address):
-    global ser
+def read_pressure(serial_address):
     try:
         ser = serial.Serial(
             port=serial_address,
@@ -32,48 +22,21 @@ def GP350_open_serial(serial_address):
             bytesize=serial.EIGHTBITS,
             timeout=2
         )
-    except serial.SerialException as e:
-        print(e)
-        return
-    return
-    
-def GP350_close_serial(serial_address):
-    global ser
-    try:
-        ser.close()
-    except serial.SerialException as e:
-        print(e)
-        return
-    return
 
-
-def GP350_read_pressure():
-    pressure = '10'
-    
-    try:   
-        read_command = "#RD\r" ## for GP350
+        read_command = "#RD\r" ##GP350
         read_command = bytes(read_command, 'utf-8')
-           
-        tries = 0
+
+        ser.write(read_command)
+        time.sleep(.3)
+        answer = ser.read_until('\r').decode('utf-8')
     
-        while pressure[0] != '0' and tries<1:
-            ser.write(read_command)
-            time.sleep(.3)
-            pressure = ''
-            ser_tries = 0
-            while ser.inWaiting() > 0 and ser_tries < 15:
-                pressure += ser.read(1).decode('utf-8')
-                ser_tries = ser_tries+1      
-            tries = tries + 1
-            time.sleep(.2)
+        pressure = float(answer.split('\r')[0].split(' ')[1])
         
-    except serial.SerialException as e:
-        print(e)
-        return 0.
-    try:
-        if pressure[0] == '0':
-            return pressure[3:-1]
-        else:
-            return float(pressure[1:-1])
+        if pressure == 9.9E9:
+            ## gauge probably off, set to nan
+            pressure = float('nan')
+        
     except:
-        return 0.
+        pressure = float('nan')
+    
+    return pressure
